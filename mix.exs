44d2edcf01364fce35df0e5c46c7388af4ef56d4
pnezis <pnezis@gmail.com>
2023-06-20 13:40:26 +0300

    update package info

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule FancyFences.MixProject do
   use Mix.Project
 
+  @repo_url "https://github.com/pnezis/fancy_fences"
+
   def project do
     [
       app: :fancy_fences,
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      description: "An earmark wrapper that post-processes code fences",
+      package: package()
     ]
   end
 
@@ -22,6 +28,14 @@ defmodule FancyFences.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.29"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Panagiotis Nezis"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @repo_url}
     ]
   end
 end
