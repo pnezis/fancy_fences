a475c17ac5e5ba987f81cb63a12a9db17172b754
pnezis <pnezis@gmail.com>
2023-08-09 10:53:32 +0300

    bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule FancyFences.MixProject do
   def project do
     [
       app: :fancy_fences,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
