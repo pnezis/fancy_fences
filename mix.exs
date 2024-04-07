d20a2537453444c725fb37125d5783204939bb1c
pnezis <pnezis@gmail.com>
2024-04-07 19:18:04 +0300

    Bump ex_doc version

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule FancyFences.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.29"}
+      {:ex_doc, "~> 0.31"}
     ]
   end
 
